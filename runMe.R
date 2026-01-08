8b2cd21bcfb1f01ddcd0d3ea6196e602c3edb71d
tati-micheletti <tati.micheletti@gmail.com>
2026-01-08 04:23:07 +0000

    Updated: added movebank check and options for cache to work (i.e., memfrac = 0) -- the last will not be needed once the next version of reproducible is in CRAN

--- runMe.R
+++ runMe.R
@@ -6,18 +6,15 @@ getOrUpdatePkg <- function(p, minVer = "0") {
 }
 getOrUpdatePkg("Require", "1.0.1.9020")
 getOrUpdatePkg("SpaDES.project", "0.1.1.9036")
-getOrUpdatePkg("terra")
 
 ################### SETUP
 
 if (SpaDES.project::user("tmichele")){ # ON BC
-  setwd("~/projects/preVal/")
   scratchPath <- Require::checkPath("~/scratch", create = TRUE)
-  terra::terraOptions(tempdir = scratchPath)
+  if (getwd() != "/home/tmichele/projects/PreVal") setwd("~/projects/preVal/")
 } 
 if (SpaDES.project::user("Tati")){  # ON MY WINDOWS MACHINE
   scratchPath <- Require::checkPath("scratch", create = TRUE)
-  terra::terraOptions(tempdir = scratchPath)
 } 
 
 runName <- "iSSA"
@@ -34,11 +31,12 @@ out <- SpaDES.project::setupProject(
     'tati-micheletti/prepLandscape@main',
     'tati-micheletti/extractLand@main'
   ),
-  options = list(spades.allowInitDuringSimInit = TRUE,
+  options = list(tempdir = scratchPath, # terra::terraOptions
+                 spades.allowInitDuringSimInit = TRUE,
                  reproducible.cacheSaveFormat = "rds",
                  gargle_oauth_email = if (user("tmichele")||user("Tati")) "tati.micheletti@gmail.com" else NULL,
                  gargle_oauth_cache = ".secrets",
-                 gargle_oauth_client_type = "installed", # Without "web", google authentication didn't work when running non-interactively!
+                 gargle_oauth_client_type = "web", # Without "web", google authentication didn't work when running non-interactively! "installed" should be used in non-server systems 
                  use_oob = TRUE, # TRUE
                  repos = "https://cloud.r-project.org",
                  spades.project.fast = FALSE,
@@ -48,32 +46,40 @@ out <- SpaDES.project::setupProject(
                  reproducible.gdalwarp = TRUE,
                  reproducible.inputPaths = if (user("tmichele")) "~/data" else paths[["inputPath"]],
                  reproducible.destinationPath = if (user("tmichele")) "~/data" else paths[["outputPath"]],
-                 reproducible.useMemoise = FALSE
+                 reproducible.useMemoise = TRUE,
+                 reproducible.showSimilar =TRUE,
+                 terra_default = list(memfrac = 0) 
   ),
   times = list(start = 2025,
                end = 2025),
   # authorizeGDrive = googledrive::drive_auth(cache = ".secrets"),
   params = list(caribouLocPrep = list(
-                       jurisdiction = "NT",
-                       herdNT = "Dehcho Boreal Woodland Caribou"),
-                prepTracks = list(
-                       rate = amt::hours(8),
-                       tolerance = amt::minutes(240),
-                       probsfilter = 0.9),
-                extractLand = list(
-                       histLandYears = 2005:2021), # go back all the way to first year of caribou data!),
-                .globals = list(
-                       .plots = c("png"),
-                       .studyAreaName=  "NT",
-                       jurisdiction = "NT",
-                       .useCache = c(".inputObjects")
-                )
-                ),
-  packages = c(#"googledrive", 'RCurl', 'XML', 'igraph', 'qs', 'usethis',
-               #"SpaDES.tools",
-               "purrr", "amt", # 1. First time to run: purrr was not installed so I added here. Restart the session after that.
+    jurisdiction = "NT",
+    herdNT = "Dehcho Boreal Woodland Caribou"),
+    prepTracks = list(
+      minyr = 2005,
+      maxyr = 2024,
+      rate = amt::hours(8),
+      tolerance = amt::minutes(240),
+      probsfilter = 0.9,
+      aggrNonAnnualData = "middle"),
+    extractLand = list(
+      histLandYears = 2008:2022,
+      checkExistingExtracted = TRUE,
+      hashExtracted = "run01",
+      .saveInitialTime = 1),
+    prepLandscape = list(
+      histLandYears = 2008:2023), # first year of caribou data that matches landcover and 5year interval
+    .globals = list(
+      .plots = c("png"),
+      .studyAreaName=  "NT",
+      jurisdiction = "NT",
+      .useCache = c(".inputObjects")
+    )
+  ),
+  packages = c("terra", "purrr", "amt",
                "PredictiveEcology/SpaDES.core@box"# # OLDER VERSIONS: 2.1.5.9022 # (>= 2.1.6.9002)
-               ),
+  ),
   useGit = "both",
   loadOrder = c(
     "caribouLocPrep",
@@ -83,7 +89,7 @@ out <- SpaDES.project::setupProject(
   )
 )
 
+source("https://raw.githubusercontent.com/tati-micheletti/PreVal/refs/heads/main/R/checkMovebankCredentials.R")
+checkMovebankCredentials(out)
+
 iSSA <- SpaDES.core::simInitAndSpades2(out)
-
-
-
