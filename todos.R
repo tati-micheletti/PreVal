88e3decf060736f13eb2298296d1acc68fdd7d9a
tati-micheletti <tati.micheletti@gmail.com>
2026-02-23 13:39:53 +0000

    Todo Updated

--- todos.R
+++ todos.R
@@ -7,12 +7,22 @@
 # [X] Julie says I do need to figure out why x2_ are NA... So I am currently in prepTracks. The problem is the function `random_steps`... But why I am currently investigating. 
 # [X] RA! I know what happened:  In the original data there are still steps with 0 length. These are creating the problem. They can only be checked, however, once the tracks are created! So that's what I am doing now in prepTracks: I clean these up, and reconstruct the data for creating the random steps. It should be good to go now. 
 # [X] Move on to preparing the data to use in a RSF or iSSA. Better have it in tables?! This is probably done in the iSSA module, but needs to be a separate module so can be used with another (i.e., RSF) model! --> No, it is done in extractLand! I just need to change the function, as I need the yearly, not 5 year version. --> This is actually done already in prepTracks. I implemented 3 different ways of adding the aggregated data
-# [ ] Run the modules and get the covariates!!!
-# [ ] Run the iSSA's as per table (Important: the model has to retain AT LEAST indv random effects. Interactions can be removed as per global models)
-# [ ] Run the RSF's as per table -- use similar structure as iSSA
+# [X] Run the modules and get the covariates!!!
+# [X] Run the iSSA's as per table (Important: the model has to retain AT LEAST indv random effects. Interactions can be removed as per global models)
+# No difference between iSSA and RSF... I decided to run NN.
+# [X] Run global NN for ranking covariates --> RESULTS ARE SOOOO COOL! A paper on their own, probably...
+# [X] Write the modules for running the experiment
+# [X] Test experiment with 6 models (2 triplets)
+# [X] Analize experiment
+# [ ] Optimize the running of the experiment
+#   [ ] cov2
+#   [ ] cov5
+#   [ ] cov10
+#   [ ] covInf
+# [ ] Run experiment
+# [ ] Analyze experiment --> Important: when plotting, really make sure the difference across the triplets is shown. We have replicates of them, so we need to use them properly.
 # [ ] Summarize the findings
 # [ ] Write the paper!
-
 
 # Desired improvements to the code:
 # 1. Anthro disturbances 
